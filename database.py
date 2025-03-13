c87842411f9cd61f0aabfc1567a90e22633b72bf
marcelnoehre <marcelnoehre@web.de>
2025-03-13 10:59:58 +0100

    general query execution

--- database.py
+++ database.py
@@ -5,11 +5,25 @@ from cryptography.fernet import Fernet
 class SecureSnippetsDB:
     def __init__(self, db_path=None):
         self.db_path = db_path or os.path.expanduser("~/snip_vault.db")
-        self._connection = sqlite3.connect(self.db_path)
-        self._cursor = self._connection.cursor()
         self._key_path = os.path.expanduser("~/.secure_snippet_key")
         self._key = self._load_key()
+        self._init_db()
 
+    def _execute_query(self, query, params=None):
+        with sqlite3.connect(self.db_path) as _connection:
+            _connection.cursor().execute(query, params or ())
+            _connection.commit()
+            _connection.close()
+
+    def _init_db(self):
+        self._execute_query("""
+            CREATE TABLE IF NOT EXISTS snippets (
+                id INTEGER PRIMARY KEY,
+                name TEXT UNIQUE,
+                data TEXT
+            )
+        """)
+    
     def _load_key(self):
         if not os.path.exists(self._key_path):
             _key = Fernet.generate_key()
@@ -19,16 +33,3 @@ class SecureSnippetsDB:
             with open(self._key_path, "rb") as _file:
                 _key = _file.read()
         return Fernet(_key)
-
-    def init_db(self):
-        self._connection = sqlite3.connect(self.db_path)
-        self._cursor = self._connection.cursor()
-        self._cursor.execute("""
-            CREATE TABLE IF NOT EXISTS snippets (
-                id INTEGER PRIMARY KEY,
-                name TEXT UNIQUE,
-                data TEXT
-            )
-        """)
-        self._connection.commit()
-        self._connection.close()
