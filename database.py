fb9b65d7a49edd6da4ea55f311387739127695af
marcelnoehre <marcelnoehre@web.de>
2025-03-13 10:43:25 +0100

    secure key management for database encryption

--- database.py
+++ database.py
@@ -1,11 +1,24 @@
 import os
 import sqlite3
+from cryptography.fernet import Fernet
 
 class SecureSnippetsDB:
     def __init__(self, db_path=None):
         self.db_path = db_path or os.path.expanduser("~/snip_vault.db")
         self._connection = sqlite3.connect(self.db_path)
         self._cursor = self._connection.cursor()
+        self._key_path = os.path.expanduser("~/.secure_snippet_key")
+        self._key = self._load_key()
+
+    def _load_key(self):
+        if not os.path.exists(self._key_path):
+            _key = Fernet.generate_key()
+            with open(self._key_path, "wb") as _file:
+                _file.write(_key)
+        else:
+            with open(self._key_path, "rb") as _file:
+                _key = _file.read()
+        return Fernet(_key)
 
     def init_db(self):
         self._connection = sqlite3.connect(self.db_path)
