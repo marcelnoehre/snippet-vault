12f8be2344f680ccdcd6f6490a1ae260d7c8a9f0
marcelnoehre <marcelnoehre@web.de>
2025-03-13 12:00:50 +0100

    fix database handling

--- database.py
+++ database.py
@@ -13,7 +13,8 @@ class SecureSnippetsDB:
 
     def _init_db(self):
         with sqlite3.connect(self.db_path) as _connection:
-            _connection.cursor().execute("""
+            _cursor = _connection.cursor()
+            _cursor.execute("""
                 CREATE TABLE IF NOT EXISTS snippets (
                     id INTEGER PRIMARY KEY,
                     name TEXT UNIQUE,
@@ -32,20 +33,21 @@ class SecureSnippetsDB:
             with open(self._key_path, "rb") as _file:
                 _key = _file.read()
         return Fernet(_key)
-    
+
     def add_snippet(self, name, data):
         with sqlite3.connect(self.db_path) as _connection:
             _cursor = _connection.cursor()
             _cursor.execute("""
                 SELECT 1 FROM snippets WHERE name = ?
             """, (name,))
-        if _cursor.fetchone():
-            self._logger.log(f"Snippet '{name}' already exists")
-        else:
-            _cursor.execute("""
-                INSERT OR REPLACE INTO snippets (name, data) VALUES (?, ?)
-            """, (name, self._key.encrypt(data.encode()).decode()))
-            self._logger.log(f"Stored Snippet '{name}'")
+            if _cursor.fetchone():
+                self._logger.log(f"Snippet '{name}' already exists")
+            else:
+                _cursor.execute("""
+                    INSERT OR REPLACE INTO snippets (name, data) VALUES (?, ?)
+                """, (name, self._key.encrypt(data.encode()).decode()))
+                _connection.commit()
+                self._logger.log(f"Stored Snippet '{name}'")
 
     def get_snippet(self, name):
         with sqlite3.connect(self.db_path) as _connection:
@@ -54,7 +56,10 @@ class SecureSnippetsDB:
                 SELECT data FROM snippets WHERE name = ?
             """, (name,))
             _data = _cursor.fetchone()
+
         if _data:
             return self._key.decrypt(_data[0].encode()).decode()
         else:
             self._logger.log(f"Snippet '{name}' not found")
+            return None
+
